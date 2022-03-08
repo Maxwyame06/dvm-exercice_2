22e7e7127fcaeb9f743b7aa64e8af10ae712a17c
Wyame <maxwyame06@gmail.com>
2022-03-08 03:40:10 -0800

    partie 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+// partie a
 const btn = document.querySelector('.btn-menu');
 
 btn.addEventListener('click', addClassToBody);
@@ -10,8 +11,26 @@ function addClassToBody(){
     //body.classList.toggle('menu-open');
     const currentState = body.getAttribute('menu-open')
     if (currentState == 'open'){
-        body.setAttribute('menu-open', 'open'),
+        body.setAttribute('menu-open', 'open');
     } else {
         body.toggleAttribute('menu-open', 'close');
     }
-}
+} 
+/*
+//partie b
+const menuLinks = document.querySelectorAll('menu__link');
+// => lienst intro, liens help
+// boucler sur mon tableau de lien
+menuLinks.forEach(function(link) {
+    //ecouter le click
+    link.addEventListener('click', onMenuClick);
+
+});
+
+function onMenuClick (e) {
+    e.preventDefault();
+    const target = e.currentTarget;
+    console.log('on menu click bien appelé')
+    console.log(e)
+    console.log(Target)
+}*/
