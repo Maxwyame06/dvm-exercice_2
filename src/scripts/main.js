aa1fa87a47cb98685f2efc0ce78405931520c881
Wyame <maxwyame06@gmail.com>
2022-03-08 01:43:49 -0800

    ajout classe open sur le body

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
-console.info('Hello world');
+const btn = document.querySelector('.btn-menu');
+
+btn.addEventListener('click', addClassToBody);
+
+function addClassToBody(){
+    console.log('la fonction addClassToBody est bien appelé');
+
+    const body = document.querySelector('body');
+    //body.classList.add('dark');
+    body.setAttribute('data-menu', 'open');
+}
